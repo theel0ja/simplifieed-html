3342f86851f28a840b1d3ed8f22999181d29af5b
Elias Ojala <ojala.s.elias@gmail.com>
2016-07-20 19:15:54 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!--
+	Simplifieed Template (v.1.0.0) by TheEl0ja
+	Link: http://theel0ja.info/
+	Downloads: http://theel0ja.info/sivupohjat
+	Authors: TheEl0ja - http://theel0ja.info/
+-->
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<title>Sivustosi.fi: Etusivu</title>
+
+		<!-- Fontit ja tyylit -->
+		<link rel="stylesheet" href="./style.css" />
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald|Ubuntu:300" />
+		<link rel="stylesheet" href="https://d2hfy7jlnfzeu2.cloudfront.net/lib/w3.min.css" />
+
+		<!-- IE-bugikorjauksia -->
+		<!--[if lt IE 9]>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/r29/html5.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<header>
+			Sivustosi.fi
+		</header>
+
+		 <nav class="w3-topnav w3-large">
+		  <a href="./">Etusivu</a>
+		  <a href="#">Linkki 1</a>
+		  <a href="#">Linkki 2</a>
+		  <a href="#">Linkki 3</a>
+		  <a href="#">Linkki 4</a>
+		</nav>
+
+		<div id="bg">
+
+			<section>
+				<span class="otsikko">Tervetuloa sivupohjaani!</span>
+				<article>
+					<p>
+						Tervetuloa <b>Simplifieed</b>-sivupohjaani! Saat käyttää sivupohjaa lähes miten vain haluat, kunhan noudatat seuraavia ehtoja:<br>
+
+						<ul>
+							<li>Saat poistaa "Layout made by <a href="http://theel0ja.info">TheEl0ja</a>"-tekstin.</li>
+							<li><b>Et</b> saa jakaa/myydä sivupohjaa muille, paitsi antamalla linkin <a href="http://theel0ja.info/sivupohjat">tänne!</a></li>
+						</ul>
+					</p>
+
+					<p><a href="javascript:void(location.href='https://validator.w3.org/check?uri='+escape(window.location));">validoi html5</a>
+					| <a href="javascript:void(location.href='https://jigsaw.w3.org/css-validator/validator?uri='+escape(window.location)+'&amp;profile=css3');">validoi css3</a>
+				</article>
+			</section>
+
+		</div>
+
+		<footer>
+			&copy; Sivustosi.fi | Layout made by <a href="https://theel0ja.info">TheEl0ja</a>
+		</footer>
+	</body>
+</html>
+<!--
+	Simplifieed Template (v.1.0.0) by TheEl0ja
+	Link: https://theel0ja.info/
+	Downloads: https://theel0ja.info/sivupohjat
+	Authors: TheEl0ja - https://theel0ja.info/
+-->
